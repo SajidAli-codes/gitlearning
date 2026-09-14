b07a41042253139ff2f19e20eecf991786f60c19
sajid ali <sajid.akhssg@gmail.com>
2026-09-14 15:59:50 +0500

    profile commit

--- frontend.py
+++ frontend.py
@@ -1,3 +1,4 @@
 print("this is the main file for the login system")
 print("this  file handle the fronted of the project")
 print("i have undo the last commit and added the new changes to the file")
+print("this is in the profile commit")
